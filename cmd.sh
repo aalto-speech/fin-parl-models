7cb74c4c503b7db1faaf444768809ae9e8d89bd4
Anja Virkkunen <anja.virkkunen@aalto.fi>
2021-09-28 11:17:30 +0300

    Update file permissions

--- cmd.sh
+++ cmd.sh
@@ -11,7 +11,7 @@
 # or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
 export basic_cmd="slurm.pl --mem 4G --time 04:00:00"
-export train_cmd="slurm.pl --mem 4G --time 01:30:00"
+export train_cmd="slurm.pl --mem 4G --time 04:00:00"
 export gmm_cmd="slurm.pl --mem 4G --time 1:30:00"
 export egs_cmd="slurm.pl --mem 2G --time 8:0:0"
 export nnet_cmd="slurm.pl --gpu 1 --mem 4G --time 00:10:00"
