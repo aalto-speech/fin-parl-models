e86a5daa4a71a616a20f9aade8aa8498f1042ebf
Anja Virkkunen <anja.virkkunen@aalto.fi>
2021-10-08 15:25:50 +0300

    Update CUDA

--- path.sh
+++ path.sh
@@ -1,9 +1,11 @@
 export PYTHONIOENCODING='utf-8'
-module load kaldi-strawberry/344c691-staticmath-gcc8.4.0-cuda10.2.89-openblas0.3.13-openfst1.6.7
-module load cuda/10.2.89
+module load kaldi-strawberry/2b1b041-staticmath-gcc8.4.0-cuda11.0.2-openblas0.3.13-openfst1.6.7
+module load cuda/11.0.2
 module load variKN
 module load sentencepiece
+
 export PATH=$PWD/utils:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD
 
 export LC_ALL=C
+
