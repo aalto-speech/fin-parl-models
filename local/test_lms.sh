46421924ebadb3fdff7c961be2591ad317995bd9
Anja Virkkunen <anja.virkkunen@aalto.fi>
2021-10-11 12:23:34 +0300

    Make LM testing more flexible.

--- local/test_lms.sh
+++ local/test_lms.sh
@@ -5,14 +5,15 @@ set -eu
 echo "$0 $@"  # Print the command line for logging
 
 stage=1
+score_only_tdnn_d=true
 
 [ -f path.sh ] && . ./path.sh # source the path.
 . parse_options.sh || exit 1;
 . cmd.sh
 
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <lm> <decode_set>"
-	echo "e.g.: $0 test_varikn.bpe1750.d0.0001 parl-dev-all"
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 <lm> <am_dir> <decode_set>"
+	echo "e.g.: $0 test_varikn.bpe1750.d0.0001 exp parl-dev-all"
 	echo
 	echo "Decode all chain models with given language model and"
 	echo "decode set."
@@ -21,10 +22,11 @@ if [ "$#" -ne 2 ]; then
 fi
 
 lm=$1
-decode_set=$2
+am_dir=$2
+decode_set=$3
 
 if [ $stage -le 1 ]; then
-	dir=exp/i/mono
+	dir=$am_dir/i/mono
 	echo "Decoding set $decode_set with monophone model $dir and LM $lm next."
 	utils/mkgraph.sh --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -34,7 +36,7 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-	dir=exp/i/tri1a
+	dir=$am_dir/i/tri1a
 	echo "Decoding set $decode_set with first triphone model $dir and LM $lm next."
 	utils/mkgraph.sh --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -44,7 +46,7 @@ if [ $stage -le 2 ]; then
 fi
 
 if [ $stage -le 3 ]; then
-	dir=exp/i/tri2a
+	dir=$am_dir/i/tri2a
 	echo "Decoding set $decode_set with second triphone model $dir and LM $lm next."
 	utils/mkgraph.sh --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -54,7 +56,7 @@ if [ $stage -le 3 ]; then
 fi
 
 if [ $stage -le 4 ]; then
-	dir=exp/i/tri3a
+	dir=$am_dir/i/tri3a
 	echo "Decoding set $decode_set with third triphone model $dir and LM $lm next."
 	utils/mkgraph.sh --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -64,7 +66,7 @@ if [ $stage -le 4 ]; then
 fi
 
 if [ $stage -le 5 ]; then
-	dir=exp/i/tri4j
+	dir=$am_dir/i/tri4j
 	echo "Decode with the last GMM."
 	utils/mkgraph.sh --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -73,9 +75,24 @@ if [ $stage -le 5 ]; then
 fi
 
 if [ $stage -le 6 ]; then
-	for model in a b c d
+	dir=$am_dir/chain/tdnn_d
+	echo "Decoding set $decode_set with AM $dir and LM $lm next."
+	utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov data/lang_$lm $dir $dir/graph_$lm
+
+	steps/nnet3/decode.sh --acwt 1.0 --post-decode-acwt 10.0 \
+			--nj 16 --cmd "$basic_cmd" \
+			$dir/graph_$lm data/${decode_set}_hires $dir/decode_${decode_set}_$lm
+fi
+
+if [ "$score_only_tdnn_d" = true ]; then
+	echo "Skipping other DNN models."
+	exit 0
+fi
+
+if [ $stage -le 7 ]; then
+	for model in a b c
 	do
-		dir=exp/chain/tdnn_$model
+		dir=$am_dir/chain/tdnn_$model
 		echo "Decoding set $decode_set with AM $dir and LM $lm next."
 		utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -85,8 +102,8 @@ if [ $stage -le 6 ]; then
 	done
 fi
 
-if [ $stage -le 7 ]; then
-	dir=exp/chain/tdnn_blstm_a_multigpu
+if [ $stage -le 8 ]; then
+	dir=$am_dir/chain/tdnn_blstm_a_multigpu
 	echo "Decoding set $decode_set with AM $dir and LM $lm next."
 	utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov data/lang_$lm $dir $dir/graph_$lm
 
@@ -99,3 +116,4 @@ if [ $stage -le 7 ]; then
 			--frames-per-chunk "140" \
 			$dir/graph_$lm data/${decode_set}_hires $dir/decode_${decode_set}_$lm
 fi
+
