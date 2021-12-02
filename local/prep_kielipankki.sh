fe95be6063affe1e5cbb931679ea06bceab9d6d8
Anja Virkkunen <anja.virkkunen@aalto.fi>
2021-12-02 11:47:13 +0200

    Kielipankki data preparation script.

---
+++ local/prep_kielipankki.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+kielipankki_root="data/kielipankki"
+
+. path.sh
+. parse_options.sh
+
+lmdatadir=data/lm_kielipankki
+mkdir -p $lmdatadir
+
+local/preprocess_lm_data.py \
+	<(gunzip -c "$kielipankki_root"/traindata.txt.gz | sed "/FILE:/d" | iconv -f LATIN1 -t UTF-8) \
+	> "$lmdatadir"/plain_text
+
+local/preprocess_lm_data.py \
+	<(gunzip -c "$kielipankki_root"/develdata.hame0008.txt.gz "$kielipankki_root"/develdata.otava_anhava.txt.gz | sed "/FILE:/d" | iconv -f LATIN1 -t UTF-8)\
+	> "$lmdatadir"/plain_text.valid
