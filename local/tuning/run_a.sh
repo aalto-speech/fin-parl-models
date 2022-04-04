ee3fecc1c55a4575b579764342823872ef75d0db
Anja <anja.virkkunen@aalto.fi>
2022-04-04 12:49:53 +0300

    Make GMM run_a.sh similar to run.sh.

--- local/tuning/run_a.sh
+++ local/tuning/run_a.sh
@@ -4,194 +4,216 @@
 # where a GMM is trained just like in the Kaldi Librispeech recipe.
 # (with added decoding steps)
 
-release_traindir=raw_data/2015-2020-kevat
+trainset=parl2015-2020-train
 stage=1
 tri_version=a
+lm_BPE=1750
+lm_scale=0.0001
+# Use the run_cleanup.sh script to clean data
+use_cleaned=false
 
 . ./cmd.sh
 . ./path.sh
 . parse_options.sh
 
-set -e -u
+set -eu
+$use_cleaned && trainset=${trainset}_cleaned
+traindir=data/${trainset}
+lm_name="test_${trainset}_varikn.bpe${lm_BPE}.d${lm_scale}"
+lmdir="data/lang_${lm_name}"
 
-if [ $stage -le 1 ]; then
-	echo "Stage 1: Get datadirs."
-  # Prep the data locally
-  mkdir -p data
-  # Copy the datadir from the release directory.
-  utils/copy_data_dir.sh $release_traindir data/train
-	# TODO: get DEV and TEST sets from somewhere
+if [[ $stage -le 1 && ! -d $traindir ]]; then
+    echo "Stage 1: Preparing all data."
+    if [[ $use_cleaned == false ]]; then
+        local/data_download.sh
+        local/data_prep.sh
+    else
+        echo "Cleaned data dir $traindir does not exist." &&
+            echo "Create cleaned data with local/run_cleanup.sh script." &&
+            exit 1
+    fi
 fi
 
 if [ $stage -le 2 ]; then
-	echo "Stage 2: Prepare lexicon."
-	# Prep lexicon
-	local/prepare_lexicon.sh data/train data/local/dict_train data/lang_train
+    echo "Stage 2: Prepare lexicon."
+    local/prepare_lexicon.sh $traindir data/local/dict_${trainset} data/lang_${trainset}
 fi
 
 if [ $stage -le 3 ]; then
-	echo "Stage 3: Prepare language model."
-	# Prepare language model
-	local/train_lm.sh --stage 0 --BPE-units 1000 \
-										--varikn-opts "--scale 0.05 --prune-scale 0.5" \
-										--traindata data/train --validdata data/parl-dev-all \
-										data/lm_data exp/varikn.bpe1000.small data/lang_test_small
+    echo "Stage 3: Prepare language model."
+    local/train_lm.sh --stage 0 --BPE-units $lm_BPE --varikn-scale $lm_scale \
+        --traindata $traindir --validdata data/parl2016-dev \
+        $trainset $lmdir
 fi
 
-
 if [ $stage -le 4 ]; then
-	echo "Stage 4: Compute features."
-  # Features
-  steps/make_mfcc.sh --cmd "$basic_cmd" --nj 32 data/train
-  steps/compute_cmvn_stats.sh data/train
-	# TODO: what are the dev and test sets called
-	#steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 data/parl-dev-seen
-  #steps/compute_cmvn_stats.sh data/parl-dev-seen
-	#steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 data/parl-dev-unseen
-  #steps/compute_cmvn_stats.sh data/parl-dev-unseen
+    echo "Stage 4: Compute features."
+    steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 $traindir
+    steps/compute_cmvn_stats.sh $traindir
+    steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 data/parl2016-dev
+    steps/compute_cmvn_stats.sh data/parl2016-dev
+    steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 data/parl2016-test
+    steps/compute_cmvn_stats.sh data/parl2016-test
+    steps/make_mfcc.sh --cmd "$basic_cmd" --nj 8 data/parl2020-test
+    steps/compute_cmvn_stats.sh data/parl2020-test
 fi
 
 if [ $stage -le 5 ]; then
-	echo "Stage 5: Make subsets."
-  # Subsets
-  utils/subset_data_dir.sh --shortest data/train 2000 data/train_2kshort
-  utils/subset_data_dir.sh data/train 5000 data/train_5k
-  utils/subset_data_dir.sh data/train 10000 data/train_10k
-  # Manually enforce some rare letters:
-	for subset in 2kshort 5k 10k; do
-		for letter in c f q w x z å; do 
-			local/enforce_letter_in_data.sh data/train "$letter" data/train_$subset
-		done
-	done
+    echo "Stage 5: Make subsets."
+    # Subsets
+    utils/subset_data_dir.sh --shortest data/train 2000 data/train_2kshort
+    utils/subset_data_dir.sh data/train 5000 data/train_5k
+    utils/subset_data_dir.sh data/train 10000 data/train_10k
+    # Manually enforce some rare letters:
+    for subset in 2kshort 5k 10k; do
+        for letter in c f q w x z å; do
+            local/enforce_letter_in_data.sh data/train "$letter" data/train_$subset
+        done
+    done
+
+    echo "Stage 5: Make subsets."
+    utils/subset_data_dir.sh --shortest $traindir 2000 ${traindir}_2kshort
+    utils/subset_data_dir.sh $traindir 5000 ${traindir}_5k
+    utils/subset_data_dir.sh $traindir 10000 ${traindir}_10k
+    # Manually enforce some rare letters:
+    for subset in 2kshort 5k 10k; do
+        for letter in c f q w x z å; do
+            local/enforce_letter_in_data.sh $traindir "$letter" ${traindir}_${subset}
+        done
+    done
+
 fi
 
 if [ $stage -le 6 ]; then
-	echo "Stage 6: Train monophone system."
-  # train a monophone system
-  steps/train_mono.sh --boost-silence 1.25 --nj 20 --cmd "$basic_cmd" \
-                      data/train_2kshort data/lang_train exp/a/mono
+    echo "Stage 6: Train monophone system with shortest 2000 utterances."
+    steps/train_mono.sh --boost-silence 1.25 --nj 8 --cmd "$basic_cmd" \
+        ${traindir}_2kshort data/lang_${trainset} exp/${trainset}/a/mono
 fi
 
 if [ $stage -le 7 ]; then
-	echo "Stage 7: Evaluate monophone system."
-	utils/mkgraph.sh data/lang_test_small exp/a/mono exp/a/mono/graph_test_small
+    echo "Stage 7: Evaluate monophone system."
+    utils/mkgraph.sh $lmdir exp/${trainset}/a/mono \
+        exp/${trainset}/a/mono/graph_${lm_name}
 
-	# Decode with triphone model
-	steps/decode.sh --cmd "$basic_cmd" --nj 8 \
-									exp/a/mono/graph_test_small data/parl-dev-all \
-									exp/a/mono/decode_parl-dev-all_test_small
+    steps/decode.sh --cmd "$basic_cmd" --nj 8 \
+        exp/${trainset}/a/mono/graph_${lm_name} data/parl2016-dev \
+        exp/${trainset}/a/mono/decode_parl2016-dev_${lm_name}
 fi
 
 if [ $stage -le 8 ]; then
-	echo "Stage 8: Train first triphone system with 5k utterances."
-  steps/align_si.sh --boost-silence 1.25 --nj 10 --cmd "$basic_cmd" \
-                    data/train_5k data/lang_train exp/a/mono exp/a/mono_ali_5k
+    echo "Stage 8: Train first triphone system with 5k utterances."
+    steps/align_si.sh --boost-silence 1.25 --nj 8 --cmd "$basic_cmd" \
+        ${traindir}_5k data/lang_${trainset} \
+        exp/${trainset}/a/mono exp/${trainset}/a/mono_ali_5k
 
-  # train a first delta + delta-delta triphone system on a subset of 5000 utterances
-  steps/train_deltas.sh --boost-silence 1.25 --cmd "$basic_cmd" \
-                        2000 10000 data/train_5k data/lang_train exp/a/mono_ali_5k exp/a/tri1a
+    steps/train_deltas.sh --boost-silence 1.25 --cmd "$basic_cmd" \
+        2000 10000 ${traindir}_5k data/lang_${trainset} \
+        exp/${trainset}/a/mono_ali_5k exp/${trainset}/a/tri1a
 fi
 
 if [ $stage -le 9 ]; then
-	echo "Stage 9: Evaluate 5k triphone system."
-	utils/mkgraph.sh data/lang_test_small exp/a/tri1a exp/a/tri1a/graph_test_small
+    echo "Stage 9: Evaluate 5k triphone system."
+    utils/mkgraph.sh $lmdir exp/${trainset}/a/tri1a \
+        exp/${trainset}/a/tri1a/graph_${lm_name}
 
-	# Decode with triphone model
-	steps/decode.sh --cmd "$basic_cmd" --nj 8 \
-									exp/a/tri1a/graph_test_small data/parl-dev-all \
-									exp/a/tri1a/decode_parl-dev-all_test_small
+    steps/decode.sh --cmd "$basic_cmd" --nj 8 \
+        exp/${trainset}/a/tri1a/graph_${lm_name} data/parl2016-dev \
+        exp/${trainset}/a/tri1a/decode_parl2016-dev_${lm_name}
 fi
 
 if [ $stage -le 10 ]; then
-	echo "Stage 10: Train second triphone system with 10k utterances."
-	steps/align_si.sh --nj 10 --cmd "$basic_cmd" data/train_10k data/lang_train exp/a/tri1a exp/a/tri1a_ali_10k
+    echo "Stage 10: Train second triphone system with 10k utterances."
+    steps/align_si.sh --nj 8 --cmd "$basic_cmd" ${traindir}_10k \
+        data/lang_${trainset} exp/${trainset}/a/tri1a \
+        exp/${trainset}/a/tri1a_ali_10k
 
-	# train an LDA+MLLT system
-	steps/train_lda_mllt.sh --cmd "$basic_cmd" --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
-													data/train_10k data/lang_train exp/a/tri1a_ali_10k exp/a/tri2a
+    steps/train_lda_mllt.sh --cmd "$basic_cmd" \
+        --splice-opts "--left-context=3 --right-context=3" \
+        2500 15000 ${traindir}_10k data/lang_${trainset} \
+        exp/${trainset}/a/tri1a_ali_10k exp/${trainset}/a/tri2a
 fi
 
 if [ $stage -le 11 ]; then
-	echo "Stage 11: Evaluate second triphone system."
-	utils/mkgraph.sh data/lang_test_small exp/a/tri2a exp/a/tri2a/graph_test_small
+    echo "Stage 11: Evaluate second triphone system."
+    utils/mkgraph.sh $lmdir exp/${trainset}/a/tri2a \
+        exp/${trainset}/a/tri2a/graph_${lm_name}
 
-	# Decode with triphone model
-	steps/decode.sh --cmd "$basic_cmd" --nj 8 \
-									exp/a/tri2a/graph_test_small data/parl-dev-all \
-									exp/a/tri2a/decode_parl-dev-all_test_small
+    steps/decode.sh --cmd "$basic_cmd" --nj 8 \
+        exp/${trainset}/a/tri2a/graph_${lm_name} data/parl2016-dev \
+        exp/${trainset}/a/tri2a/decode_parl2016-dev_${lm_name}
 fi
 
 if [ $stage -le 12 ]; then
-	echo "Stage 12: Train third triphone system with 10k utts and LDA+MLLT+SAT."
-	steps/align_si.sh --nj 10 --cmd "$basic_cmd" --use-graphs true \
-										data/train_10k data/lang_train exp/a/tri2a exp/a/tri2a_ali_10k
+    echo "Stage 12: Train third triphone system with 10k utts and LDA+MLLT+SAT."
+    steps/align_si.sh --nj 8 --cmd "$basic_cmd" --use-graphs true \
+        ${traindir}_10k data/lang_${trainset} \
+        exp/${trainset}/a/tri2a exp/${trainset}/a/tri2a_ali_10k
 
-	# train an LDA+MLLT+SAT system
-	steps/train_sat.sh --cmd "$basic_cmd" \
-													2500 15000 \
-													data/train_10k data/lang_train exp/a/tri2a_ali_10k exp/a/tri3a
+    steps/train_sat.sh --cmd "$basic_cmd" \
+        2500 15000 ${traindir}_10k data/lang_${trainset} \
+        exp/${trainset}/a/tri2a_ali_10k exp/${trainset}/a/tri3a
 fi
 
 if [ $stage -le 13 ]; then
-	echo "Stage 13: Evaluate third triphone system."
-	utils/mkgraph.sh data/lang_test_small exp/a/tri3a exp/a/tri3a/graph_test_small
+    echo "Stage 13: Evaluate third triphone system."
+    utils/mkgraph.sh $lmdir exp/${trainset}/a/tri3a \
+        exp/${trainset}/a/tri3a/graph_${lm_name}
 
-	# Decode with triphone model
-	steps/decode_fmllr.sh --cmd "$basic_cmd" --nj 16 \
-									exp/a/tri3a/graph_test_small data/parl-dev-all \
-									exp/a/tri3a/decode_parl-dev-all_test_small
+    steps/decode_fmllr.sh --cmd "$basic_cmd" --nj 8 \
+        exp/${trainset}/a/tri3a/graph_${lm_name} data/parl2016-dev \
+        exp/${trainset}/a/tri3a/decode_parl2016-dev_${lm_name}
 fi
 
-
 if [ $stage -le 14 ]; then
-	num_leaves=7000
-	num_gauss=150000
-	num_iters=35
-	realign_iters="10 20 30"
-	fmllr_iters="2 4 6 12"
-	if [ $tri_version == b ]; then 
-		num_leaves=14000
-		num_gauss=150000
-	fi
-	if [ $tri_version == c ]; then 
-		num_leaves=14000
-		num_gauss=200000
-	fi
-	if [ $tri_version == d ]; then
-		num_iters=70
-		realign_iters="10 20 30 40 50 60"
-		fmllr_iters="2 4 6 12 36 42 48"
-	fi
-	if [ $tri_version == e ]; then
-		num_leaves=14000
-		num_gauss=200000
-		num_iters=70
-		realign_iters="10 20 30 40 50 60"
-		fmllr_iters="2 4 6 12 36 42 48"
-	fi
-	echo "Stage 14: Train fourth triphone system with full data (also LDA+MLLT+SAT)."
-	echo "Using configuration tri4$tri_version with $num_leaves leaves, $num_gauss gaussians,"
-	echo "$num_iters iterations, $realign_iters realign iterations, and $fmllr_iters fmllr iterations."
+    num_leaves=7000
+    num_gauss=150000
+    num_iters=35
+    realign_iters="10 20 30"
+    fmllr_iters="2 4 6 12"
+    if [ $tri_version == b ]; then
+        num_leaves=14000
+        num_gauss=150000
+    fi
+    if [ $tri_version == c ]; then
+        num_leaves=14000
+        num_gauss=200000
+    fi
+    if [ $tri_version == d ]; then
+        num_iters=70
+        realign_iters="10 20 30 40 50 60"
+        fmllr_iters="2 4 6 12 36 42 48"
+    fi
+    if [ $tri_version == e ]; then
+        num_leaves=14000
+        num_gauss=200000
+        num_iters=70
+        realign_iters="10 20 30 40 50 60"
+        fmllr_iters="2 4 6 12 36 42 48"
+    fi
 
-	if [ ! -d exp/a/tri3a_ali_full ]; then
-		steps/align_fmllr.sh --nj 120 --cmd "$basic_cmd" \
-										data/train data/lang_train exp/a/tri3a exp/a/tri3a_ali_full
-	fi
+    echo "Stage 14: Train fourth triphone system with full data (also LDA+MLLT+SAT)."
+    echo "Using configuration tri4$tri_version with $num_leaves leaves, "
+    echo "$num_gauss gaussians, $num_iters iterations, $realign_iters realign iterations,"
+    echo "and $fmllr_iters fmllr iterations."
 
-	# train an LDA+MLLT+SAT system
-	steps/train_sat.sh --cmd "$basic_cmd" --num-iters $num_iters \
-													--realign-iters "$realign_iters" --fmllr-iters "$fmllr_iters" \
-													$num_leaves $num_gauss \
-													data/train data/lang_train exp/a/tri3a_ali_full exp/a/tri4$tri_version
+    if [ ! -d exp/${trainset}/a/tri3a_ali_full ]; then
+        steps/align_fmllr.sh --nj 8 --cmd "$basic_cmd" \
+            $traindir data/lang_${trainset} \
+            exp/${trainset}/a/tri3a exp/${trainset}/a/tri3a_ali_full
+    fi
+
+    steps/train_sat.sh --cmd "$basic_cmd" --num-iters $num_iters \
+        --realign-iters "$realign_iters" --fmllr-iters "$fmllr_iters" \
+        $num_leaves $num_gauss $traindir data/lang_${trainset} \
+        exp/${trainset}/a/tri3a_ali_full exp/${trainset}/a/tri4$tri_version
 fi
 
 if [ $stage -le 15 ]; then
-	echo "Stage 15: Evaluate fourth triphone system."
-	utils/mkgraph.sh data/lang_test_small exp/a/tri4$tri_version exp/a/tri4$tri_version/graph_test_small
+    echo "Stage 15: Evaluate fourth triphone system."
+    utils/mkgraph.sh $lmdir exp/${trainset}/a/tri4$tri_version \
+        exp/${trainset}/a/tri4$tri_version/graph_${lm_name}
 
-	# Decode with triphone model
-	steps/decode_fmllr.sh --cmd "$basic_cmd" --nj 16 \
-									exp/a/tri4$tri_version/graph_test_small data/parl-dev-all \
-									exp/a/tri4$tri_version/decode_parl-dev-all_test_small
+    steps/decode_fmllr.sh --cmd "$basic_cmd" --nj 8 \
+        exp/${trainset}/a/tri4$tri_version/graph_${lm_name} data/parl2016-dev \
+        exp/${trainset}/a/tri4$tri_version/decode_parl2016-dev_${lm_name}
 fi
-
