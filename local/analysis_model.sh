5789853ce5672e3092321ad0d0d523c88c8214f9
Anja <anja.virkkunen@aalto.fi>
2022-04-05 16:10:47 +0300

    Edit analysis_model.sh for publication.

--- local/analysis_model.sh
+++ local/analysis_model.sh
@@ -1,69 +1,63 @@
 #!/usr/bin/env bash
+
+# Copyright 2022 Anja Virkkunen, Aku Rouhe
+# Apache 2.0
+
 # Train a 100h model for analysis and decode rest of the data with it
 
-set -euo pipefail
-
 stage=1
+analysis_set=parl2008-2020-train
+
+echo "$0 $@"
 
 . path.sh
 . parse_options.sh
 
-echo "$0 $@"
+set -euo pipefail
 
-traindata_dir=data/combined_comparison/parl-train-2008-2020-kevat_40k
+traindir=data/${analysis_set}_40k
+transcript_lm=test_${analysis_set}_40k_varikn.bpe1750.d0.0001
 
 if [ $stage -le 1 ]; then
-	echo "Make a 100 h subset (40k samples result in approx. 100 hours)."
-	utils/subset_data_dir.sh --speakers data/combined_comparison/parl-train-2008-2020-kevat \
-														40000 $traindata_dir 
+    echo "Make a 100 h subset (40k samples result in approx. 100 hours)."
+    utils/subset_data_dir.sh --speakers data/$analysis_set 40000 $traindir
 fi
 
 if [ $stage -le 2 ]; then
-	echo "Train a transcript LM from the 100 h subset."
-  local/train_lm.sh --stage 0 --BPE-units 1750 --varikn-scale 0.0001 \
-										--traindata $traindata_dir \ 
-										--validdata data/parl-dev-all \
-										parl-train-2008-2020-kevat_40k \
-										data/lang_test_parl-train-2008-2020-kevat_40k_varikn.bpe1750.d0.0001
+    echo "Train a transcript LM from the 100 h subset."
+    local/train_lm.sh --stage 0 --BPE-units 1750 --varikn-scale 0.0001 \
+        --traindata $traindir \ 
+    --validdata data/parl2016-dev \
+        ${analysis_set}_40k \
+        data/lang_$transcript_lm
 fi
 
 if [ $stage -le 3 ]; then
-	echo "Train TDNN model from the 100 hours of data."
-	local/combined_comparison/run_chain_common.sh --train-set parl-train-2008-2020-kevat_40k --nj-align 20
+    echo "Train TDNN model from the 100 hours of data."
+    local/chain/run_chain_common.sh --trainset ${analysis_set}_40k
 
-	local/combined_comparison/run_tdnn_d.sh --train-set parl-train-2008-2020-kevat_40k \
-																					--lm test_parl-train-2008-2020-kevat_40k_varikn.bpe1750.d0.0001
+    local/chain/tuning/run_tdnn_d.sh --trainset ${analysis_set}_40k --lm $transcript_lm
 fi
 
 if [ $stage -le 4 ]; then
-	echo "From remaining data, select speakers not in the 100 hour subset."
+    echo "From remaining data, select speakers not in the 100 hour subset."
 
-	tmpdir=$(mktemp -d)
-  awk '{print $1}' $traindata_dir/spk2utt > $tmpdir/trainset_speakers.list
-  awk '{print $1}' data/combined_comparison/parl-train-2008-2020-kevat/spk2utt > $tmpdir/all_speakers.list
-	grep -vf $tmpdir/trainset_speakers.list $tmpdir/all_speakers.list > $tmpdir/non-train_speakers.list
-  
-	utils/subset_data_dir.sh --spk-list $tmpdir/non-train_speakers.list \
-														data/combined_comparison/parl-train-2008-2020-kevat_hires \
-														data/combined_comparison/analysis-data_hires
-	rm -r $tmpdir
+    tmpdir=$(mktemp -d)
+    awk '{print $1}' $traindir/spk2utt >$tmpdir/trainset_speakers.list
+    awk '{print $1}' data/${analysis_set}/spk2utt >$tmpdir/all_speakers.list
+    grep -vf $tmpdir/trainset_speakers.list $tmpdir/all_speakers.list >$tmpdir/non-train_speakers.list
+
+    utils/subset_data_dir.sh --spk-list $tmpdir/non-train_speakers.list \
+        data/${analysis_set}_hires \
+        data/${analysis_set}_analysis_hires
+    rm -r $tmpdir
 fi
 
 if [ $stage -le 5 ]; then
-	echo "Decode the remaining data with an in-domain transcript LM."
-	steps/nnet3/decode.sh --stage 2 --acwt 1.0 --post-decode-acwt 10.0 \
-												--nj 128 --cmd "slurm.pl --mem 2G --time 24:00:00" \
-												exp/combined_comparison/chain/tdnn_d_40k/graph_test_parl-train-2008-2020-kevat_40k_varikn.bpe1750.d0.0001 \
-												data/combined_comparison/analysis-data_hires \
-												exp/combined_comparison/chain/tdnn_d_40k/decode_analysis-data_test_parl-train-2008-2020-kevat_40k_varikn.bpe1750.d0.0001
+    echo "Decode the remaining data with an in-domain transcript LM."
+    steps/nnet3/decode.sh --stage 2 --acwt 1.0 --post-decode-acwt 10.0 \
+        --nj 8 --cmd "run.pl" \
+        exp/${analysis_set}_40k/chain/tdnn_d/graph_$transcript_lm \
+        data/${analysis_set}_analysis_hires \
+        exp/${analysis_set}_40k/chain/tdnn_d/decode_${analysis_set}_analysis_$transcript_lm
 fi
-
-if [ $stage -le 6 ]; then
-	echo "Decode the remaining data with an out-of-domain LM."
-	steps/nnet3/decode.sh --acwt 1.0 --post-decode-acwt 10.0 \
-												--nj 128 --cmd "slurm.pl --mem 12G --time 36:00:00" \
-												exp/combined_comparison/chain/tdnn_d_40k/graph_test_5gram-kielipankki.bpe19000.d0.0001 \
-												data/combined_comparison/analysis-data_hires \
-												exp/combined_comparison/chain/tdnn_d_40k/decode_analysis-data_test_5gram-kielipankki.bpe19000.d0.0001
-fi
-
