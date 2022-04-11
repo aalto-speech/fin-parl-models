8b04e6980b9eec19ad1bb1cc985475255c892271
Anja <anja.virkkunen@aalto.fi>
2022-04-11 19:50:24 +0300

    Edit batch_rescore.sh for final publication.

--- local/batch_rescore.sh
+++ local/batch_rescore.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --time=4:0:0
-#SBATCH --mem-per-cpu=16G
-#SBATCH --output=rescore_%A.out
-#SBATCH -c 20
-#SBATCH -p batch-csl,batch-skl
 
-for data in parl-dev-all; do
-	for am in exp/chain/tdnn_d; do
-		steps/lmrescore_const_arpa.sh data/lang_test_5gram-kielipankki.bpe19000.d0.0001 \
-									  data/lang_test_kielipankki.bpe19000.d0.0001_carpa \
-									  data/${data}_hires \
-									  ${am}/decode_${data}_test_5gram-kielipankki.bpe19000.d0.0001 \
-									  ${am}/decode_${data}_test_kielipankki.bpe19000.d0.0001_rescore
-	done
+# Copyright 2022 Anja Virkkunen, Aku Rouhe
+# Apache 2.0
+
+# Rescore decoding results.
+
+for decoded_data in parl2016-dev parl2016-test parl2020-test; do
+    for am_train_data in parl2015-2020-train parl2008-2016-train parl2008-2020-train; do
+        am=exp/${am_train_data}/chain/tdnn_d
+        steps/lmrescore_const_arpa.sh data/lang_test_5gram-kielipankki.bpe19000.d0.0001 \
+            data/lang_test_kielipankki.bpe19000.d0.0001_carpa \
+            data/${decoded_data}_hires \
+            ${am}/decode_${decoded_data}_test_5gram-kielipankki.bpe19000.d0.0001 \
+            ${am}/decode_${decoded_data}_test_kielipankki.bpe19000.d0.0001_rescore
+    done
 done
-
